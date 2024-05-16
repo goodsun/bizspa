9eed9783c76e257f99f503c5004f7471de4e83cf
goodsun <goodsun0317@gmail.com>
2024-05-16 23:55:10 +0900

    API修正

--- src/module/connect/abi.ts
+++ src/module/connect/abi.ts
@@ -373,6 +373,11 @@ export const ABIS = {
     {
       inputs: [
         {
+          internalType: "address",
+          name: "donateManageAddress",
+          type: "address",
+        },
+        {
           internalType: "string",
           name: "name",
           type: "string",
@@ -381,16 +386,6 @@ export const ABIS = {
           internalType: "string",
           name: "symbol",
           type: "string",
-        },
-        {
-          internalType: "address",
-          name: "creator",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "feeRate",
-          type: "uint256",
         },
       ],
       stateMutability: "nonpayable",
@@ -447,204 +442,6 @@ export const ABIS = {
       type: "event",
     },
     {
-      inputs: [
-        {
-          internalType: "address",
-          name: "to",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
-        },
-      ],
-      name: "approve",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
-        },
-      ],
-      name: "burn",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "to",
-          type: "address",
-        },
-        {
-          internalType: "string",
-          name: "metaUrl",
-          type: "string",
-        },
-      ],
-      name: "mint",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "from",
-          type: "address",
-        },
-        {
-          internalType: "address",
-          name: "to",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
-        },
-      ],
-      name: "safeTransferFrom",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "from",
-          type: "address",
-        },
-        {
-          internalType: "address",
-          name: "to",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
-        },
-        {
-          internalType: "bytes",
-          name: "data",
-          type: "bytes",
-        },
-      ],
-      name: "safeTransferFrom",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "operator",
-          type: "address",
-        },
-        {
-          internalType: "bool",
-          name: "approved",
-          type: "bool",
-        },
-      ],
-      name: "setApprovalForAll",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "minter",
-          type: "address",
-        },
-        {
-          internalType: "address",
-          name: "creator",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "feeRate",
-          type: "uint256",
-        },
-      ],
-      name: "setConfig",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "amount",
-          type: "uint256",
-        },
-      ],
-      name: "settlement",
-      outputs: [],
-      stateMutability: "payable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
-        },
-        {
-          internalType: "string",
-          name: "metaUrl",
-          type: "string",
-        },
-      ],
-      name: "setTokenURI",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
-        },
-      ],
-      name: "setUnDraft",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "unlockPrice",
-          type: "uint256",
-        },
-      ],
-      name: "setUnlockPrice",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
       anonymous: false,
       inputs: [
         {
@@ -670,44 +467,16 @@ export const ABIS = {
       type: "event",
     },
     {
-      inputs: [
+      inputs: [],
+      name: "INVERSE_BASIS_POINT",
+      outputs: [
         {
-          internalType: "address",
-          name: "from",
-          type: "address",
-        },
-        {
-          internalType: "address",
-          name: "to",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
+          internalType: "uint16",
+          name: "",
+          type: "uint16",
         },
       ],
-      name: "transferFrom",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "tokenId",
-          type: "uint256",
-        },
-        {
-          internalType: "address",
-          name: "donor",
-          type: "address",
-        },
-      ],
-      name: "unlock",
-      outputs: [],
-      stateMutability: "payable",
+      stateMutability: "view",
       type: "function",
     },
     {
@@ -718,82 +487,6 @@ export const ABIS = {
           internalType: "address",
           name: "",
           type: "address",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "_donationAmount",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "_donationDate",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "_donor",
-      outputs: [
-        {
-          internalType: "address",
-          name: "",
-          type: "address",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "_draftId",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
         },
       ],
       stateMutability: "view",
@@ -813,48 +506,34 @@ export const ABIS = {
       type: "function",
     },
     {
+      inputs: [],
+      name: "_usePoint",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
       inputs: [
         {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "_locked",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "_minter",
-      outputs: [
-        {
           internalType: "address",
-          name: "",
+          name: "to",
           type: "address",
         },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "_unlockPrice",
-      outputs: [
         {
           internalType: "uint256",
-          name: "",
+          name: "tokenId",
           type: "uint256",
         },
       ],
-      stateMutability: "view",
+      name: "approve",
+      outputs: [],
+      stateMutability: "nonpayable",
       type: "function",
     },
     {
@@ -884,6 +563,19 @@ export const ABIS = {
           type: "uint256",
         },
       ],
+      name: "burn",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
       name: "getApproved",
       outputs: [
         {
@@ -900,27 +592,14 @@ export const ABIS = {
       name: "getInfo",
       outputs: [
         {
-          internalType: "address",
+          internalType: "string",
           name: "",
-          type: "address",
+          type: "string",
         },
         {
           internalType: "uint256",
           name: "",
           type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "INVERSE_BASIS_POINT",
-      outputs: [
-        {
-          internalType: "uint16",
-          name: "",
-          type: "uint16",
         },
       ],
       stateMutability: "view",
@@ -948,6 +627,29 @@ export const ABIS = {
         },
       ],
       stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "string",
+          name: "metaUrl",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "feeRate",
+          type: "uint256",
+        },
+      ],
+      name: "mint",
+      outputs: [],
+      stateMutability: "nonpayable",
       type: "function",
     },
     {
@@ -1038,6 +740,88 @@ export const ABIS = {
     {
       inputs: [
         {
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+        {
+          internalType: "bytes",
+          name: "data",
+          type: "bytes",
+        },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "operator",
+          type: "address",
+        },
+        {
+          internalType: "bool",
+          name: "approved",
+          type: "bool",
+        },
+      ],
+      name: "setApprovalForAll",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "usePoint",
+          type: "uint256",
+        },
+      ],
+      name: "setConfig",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
           internalType: "bytes4",
           name: "interfaceId",
           type: "bytes4",
@@ -1071,6 +855,49 @@ export const ABIS = {
       inputs: [
         {
           internalType: "uint256",
+          name: "index",
+          type: "uint256",
+        },
+      ],
+      name: "tokenByIndex",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "index",
+          type: "uint256",
+        },
+      ],
+      name: "tokenOfOwnerByIndex",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
           name: "tokenId",
           type: "uint256",
         },
@@ -1084,6 +911,42 @@ export const ABIS = {
         },
       ],
       stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "totalSupply",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "transferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
       type: "function",
     },
   ],
