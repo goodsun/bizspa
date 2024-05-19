44dab2e2e4dae14bbe7e088fcaac700a36e8d126
goodsun <goodsun0317@gmail.com>
2024-05-19 20:08:28 +0900

    markd インストール

--- src/module/snipet/article.ts
+++ src/module/snipet/article.ts
@@ -2,11 +2,15 @@ import utils from "../common/util";
 import { CONST } from "../../module/common/const";
 import { router } from "../../module/common/router";
 
-const getMdPath = () => {
+const getMdPath = async () => {
   CONST.ARTICLE_REPO_URL;
   const path = router.getParams();
   const mdpath = CONST.ARTICLE_REPO_URL + "md/" + path + ".md";
   console.log("MDPATH:" + mdpath);
+  const mdBody = await fetch(mdpath).then((response) => {
+    return response.text();
+  });
+  console.log(mdBody);
 };
 
 const article = {
